8d28a72af068a48b23f8136d94c29a4d8201bc04
Ian.Z <ianzhuo@msn.com>
2019-04-03 16:11:48 +0800

    first commit

---
+++ Deploy.bat
@@ -0,0 +1,2 @@
+xcopy .\Rime\*.* /S %AppData%\Rime\
+"C:\Program Files (x86)\Rime\weasel-0.13.0\WeaselDeployer.exe" /deploy
---
+++ Install.bat
@@ -0,0 +1,4 @@
+del %AppData%\Rime\*.* /Q
+rmdir %AppData%\RIme\opencc /S /Q
+xcopy .\Rime\*.* /S %AppData%\Rime\
+"C:\Program Files (x86)\Rime\weasel-0.13.0\WeaselDeployer.exe" /deploy
